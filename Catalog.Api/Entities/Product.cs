17f2d6ee6bd10c20f31a9a5686dff9475176d470
Yael Chavez <you400@gmail.com>
2022-10-17 15:52:18 -0500

    Add project files.

---
+++ Catalog.Api/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Api.Entities;
+
+public class Product
+{
+
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Summary { get; set; } = null!;
+
+}
+
