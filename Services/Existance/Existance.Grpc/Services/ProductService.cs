6a13d99755f60a16f662886ba280d236f033ff3a
Yael Chavez <you400@gmail.com>
2022-10-27 15:42:21 -0500

    grpc

---
+++ Services/Existance/Existance.Grpc/Services/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using Existance.Grpc.Protos;
+using Grpc.Core;
+
+namespace Existance.Grpc.Services
+{
+    public class ProductService : ExistanceService.ExistanceServiceBase
+    {
+        public override  Task<ExistanceResponse> CheckExistance(CheckExistanceRequest request, ServerCallContext context)
+        {
+            var rdn = new Random();
+            return Task.FromResult(new ExistanceResponse { Existance = rdn.Next(0,100) });
+        }
+    }
+}
